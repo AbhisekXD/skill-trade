8e0d2f68ee08d06907522d5a2726085cda4eef8b
AbhisekXD <abhiseknayak84@gmail.com>
2025-06-20 00:07:31 +0530

    stopped tsx

--- skilltrade-frontend/src/api-client/models/index.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './Barter';
-export * from './BarterRequestDTO';
-export * from './LoginDTO';
-export * from './RegisterDTO';
-export * from './Skill';
-export * from './SkillDTO';
-export * from './User';
