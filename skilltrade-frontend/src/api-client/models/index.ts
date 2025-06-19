32635ab2882b89abdd482fdff301797aeccd2d7c
AbhisekXD <abhiseknayak84@gmail.com>
2025-06-19 23:53:38 +0530

    open api apis integration react

---
+++ skilltrade-frontend/src/api-client/models/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Barter';
+export * from './BarterRequestDTO';
+export * from './LoginDTO';
+export * from './RegisterDTO';
+export * from './Skill';
+export * from './SkillDTO';
+export * from './User';
